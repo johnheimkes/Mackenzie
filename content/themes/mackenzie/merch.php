2a9e56fdfa568f0e44e4ed8fff0d40c7af3189d3
alexcarrollmn <alexcarroll.mn@gmail.com>
2012-11-08 20:47:10 -0600

    cleaning some code

--- content/themes/mackenzie/merch.php
+++ content/themes/mackenzie/merch.php
@@ -28,8 +28,6 @@
 	?>
 		<div class="merch-item">
 			<?php if (has_post_thumbnail( $post->ID ) ): ?>
-				
-			
 				<?php 
 					$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); 
 				?>
